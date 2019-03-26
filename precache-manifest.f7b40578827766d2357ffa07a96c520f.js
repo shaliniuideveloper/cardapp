900b2d28c97db37b8a154985e071060a6a65fb45
shalini_kulkarni <shaliniuideveloper@gmail.com>
2019-03-26 09:16:23 +0530

    Updates

---
+++ precache-manifest.f7b40578827766d2357ffa07a96c520f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "25b4e634dae07d5d77ebe67a3d0743b2",
+    "url": "/cardapp/static/media/tile.25b4e634.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cardapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a3f011a74b71cac77f0134976aad150",
+    "url": "/cardapp/static/media/logo.2a3f011a.png"
+  },
+  {
+    "revision": "cd527e47a9ec2ae2b572",
+    "url": "/cardapp/static/js/runtime~main.4155d4e0.js"
+  },
+  {
+    "revision": "273cf345660e9e8a5d90",
+    "url": "/cardapp/static/js/main.e22e7cb6.chunk.js"
+  },
+  {
+    "revision": "2b66e7a8a9e8ac729df8",
+    "url": "/cardapp/static/js/2.321f2247.chunk.js"
+  },
+  {
+    "revision": "273cf345660e9e8a5d90",
+    "url": "/cardapp/static/css/main.dba3301d.chunk.css"
+  },
+  {
+    "revision": "5729777665877e358b7cb1ce641ecbd3",
+    "url": "/cardapp/index.html"
+  }
+];
